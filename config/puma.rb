c12ef33b6d35e6b856e0cc433f656aafe4b766b8
Simon Cozens <simon@simon-cozens.org>
2017-10-19 09:49:15 +1100

    Maybe not with mongoid, eh.

--- config/puma.rb
+++ config/puma.rb
@@ -8,7 +8,7 @@ rackup      DefaultRackup
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
-on_worker_boot do
-  ActiveRecord::Base.establish_connection
-end
+#on_worker_boot do
+  #ActiveRecord::Base.establish_connection
+#end
 
