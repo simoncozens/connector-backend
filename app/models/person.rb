418079ff9fff7474629cf7c2cbddd800debf7a99
Simon Cozens <simon@simon-cozens.org>
2017-11-02 13:03:48 +1100

    Add all the Elasticsearch stuff

--- app/models/person.rb
+++ app/models/person.rb
@@ -6,6 +6,7 @@ end
 class Person
   include Mongoid::Document
   include Mongoid::Timestamps
+  include Elasticsearch::Model
 
 
   paginates_per 10
@@ -28,6 +29,10 @@ class Person
   field :salt, type: String, internal: true
   has_many :follows, :dependent => :destroy
   field :last_visited, type: Array, internal: true # Do this as array of IDs for simplicity
+
+  def as_indexed_json(options={})
+    as_json(only: Person.searchable_fields.keys-["_id"])
+  end
 
   def field_viewable?(field,other)
     return false if field.options[:internal]
@@ -67,5 +72,67 @@ class Person
       clause[:$text] = { :$search => params["fts"] }
     end
     self.where(clause)
+  end
+
+  # Elasticsearch stuff
+
+  # Define the index
+  settings do
+    mappings dynamic: 'false' do
+      indexes :id, index: 'not_analyzed'
+      indexes :roles, type: 'string'
+      indexes :name, type: 'string'
+      indexes :intro_bio, type: 'string'
+      indexes :preferred_contact, index: 'not_analyzed', type: "keyword"
+      indexes :affiliations, type: 'nested' do
+        indexes :organisation
+        indexes :position
+        indexes :website
+      end
+      indexes :gender, index: 'not_analyzed'
+      indexes :country, index: 'not_analyzed', type: "keyword"
+      indexes :memberships, index: 'not_analyzed', type: "keyword"
+      indexes :experience, index: 'not_analyzed', type: "keyword"
+      indexes :regions, index: 'not_analyzed', type: "keyword"
+    end
+  end
+
+  def self.aggs
+    return {
+      experience: { terms: { field: "experience" } },
+      regions: { terms: { field: "regions" } },
+      country: { terms: { field: "country" } }
+    }
+  end
+
+  def self.elasticsearch_search(q)
+    if q.class == String
+      q = { query_string: { query: q }}
+    end
+    # Let's just make this a bit friendlier
+    r = search(query: q, aggregations: Person.aggs)
+    aggs = {}
+    ["experience", "regions", "country"].each do |agg|
+      aggs[agg] = r.aggregations[agg].buckets.map {|x| {x["key"] => x.doc_count}}.reduce Hash.new, :merge
+    end
+    return Hashie::Mash.new({
+      response: r,
+      total_records: r.results.total,
+      aggregations: aggs
+    })
+  end
+
+  def similar
+    q = { more_like_this: {
+      min_doc_freq: 1,
+      min_term_freq: 1,
+      fields: ["experience", "regions", "country", "memberships"],
+      like: [ {
+        _index: __elasticsearch__.index_name,
+        _type: __elasticsearch__.document_type,
+        _id: id.to_s
+        }]
+      } }
+    return Person.elasticsearch_search(q)
   end
 end
