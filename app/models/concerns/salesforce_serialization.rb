fc898ed274b3d0e0a63a68e2842833e1190c7e1b
Simon Cozens <simon@simon-cozens.org>
2017-12-01 12:55:28 +1100

    Allow bringing in data from salesforce

--- app/models/concerns/salesforce_serialization.rb
+++ app/models/concerns/salesforce_serialization.rb
@@ -17,7 +17,10 @@ module SalesforceSerialization
       end
     },
     from_salesforce: -> sf_person, mongo_person {
-      mongo_person.affiliatons
+      mongo_person.affiliations = [
+        {organisation: "XXX", website: sf_person["Organization_Website__c"], position: sf_person["Title"]},
+        {organisation: sf_person["Org_2_Temp__c"], website: sf_person["Org_2_Website__c"], position: sf_person["Org_2_Website__c"]}
+      ]
     }
   }
 
@@ -45,7 +48,31 @@ module SalesforceSerialization
     UpdateSalesforceJob.perform_later(sf_updates)
   end
 
+  def from_salesforce(sf_user)
+    Person.fields.select {|k,f| f.options[:salesforce]}.each do |k,v|
+      sf_opt = v.options[:salesforce]
+      if sf_opt.is_a?(String)
+        write_attribute(k, sf_user[sf_opt])
+      elsif sf_opt.is_a?(Hash)
+        sf_opt[:from_salesforce].call(sf_user, self)
+      end
+    end
+  end
+
   included do
     before_save :to_salesforce
+  end
+
+  class_methods do
+    def new_from_salesforce(id)
+      p = Person.new
+      p.salesforce_id = id
+      p.from_salesforce(p.sf_person)
+      p.password = p.password_confirmation = SecureRandom.uuid # XXX?
+      Person.skip_callback(:save, :before, :to_salesforce)
+      p.save
+      Person.set_callback(:save, :before, :to_salesforce)
+      return p
+    end
   end
 end
