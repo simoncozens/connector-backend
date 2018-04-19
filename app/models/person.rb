989f54b4d98aef641fef1d72012b36724669b185
Simon Cozens <simon@simon-cozens.org>
2018-04-19 07:49:51 +0200

    Store short bio

--- app/models/person.rb
+++ app/models/person.rb
@@ -30,6 +30,7 @@ class Person
   field :facebook_id, type: String, salesforce: "Facebook__c"
   field :lausanne_title, type: String, salesforce: "Lausanne_Leadership_Title__c"
   field :intro_bio, type: String
+  field :short_bio, type: String
   field :joined_lausanne, type: String
   field :preferred_contact, type: String
   field :languages_spoken, type: String, salesforce: "Language_Ability__c"
