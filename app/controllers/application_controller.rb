56a8d02557715f7a37b14dbdaa299adfab179092
Simon Cozens <simon@simon-cozens.org>
2018-06-08 12:35:29 +1000

    Send all errors as JSON, not HTML.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,26 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session # We're authenticated anyway
   skip_before_action :verify_authenticity_token
 
+  rescue_from StandardError do |exception|
+    # Handle only JSON requests
+    raise unless request.format.json?
+
+    err = {error: exception.message}
+
+    err[:backtrace] = exception.backtrace.select do |line|
+      # filter out non-significant lines:
+      %w(/gems/ /rubygems/ /lib/ruby/).all? do |litter|
+         not line.include?(litter)
+      end
+    end if Rails.env.development? and exception.is_a? Exception
+
+    # duplicate exception output to console:
+    STDERR.puts ['ERROR:', err[:error], '']
+                    .concat(err[:backtrace] || []).join "\n"
+
+    render :json => err, :status => 500
+  end
+
   def validate_token!
     begin
       TokenProvider.valid?(token)
